568a8714c6661947029358b7208f4fabafa0e0da
Chase Granberry <chase@logflare.app>
2022-05-17 07:39:22 -0700

    Backoff

--- lib/looper.ex
+++ lib/looper.ex
@@ -4,8 +4,8 @@ defmodule LoadFestBookClub.Looper do
   alias LoadFestBookClub.Client
 
   @url Application.get_env(:load_fest_book_club, __MODULE__)[:url]
-  @max_concurrency 100
-  @stream_len 1_000
+  @max_concurrency 10
+  @stream_len 100
   @every 0
 
   def start_link(args \\ []) do
