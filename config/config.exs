297c08df81365af907fb066ed149e1e53f6dc9e8
Chase Granberry <chase@logflare.app>
2022-05-17 10:10:44 -0700

    Don't use release.exs

--- config/config.exs
+++ config/config.exs
@@ -1,5 +1,8 @@
 import Config
 
-config :load_fest_book_club, LoadFestBookClub.Looper, stream_len: 1000
+config :load_fest_book_club, LoadFestBookClub.Looper,
+  stream_len: 1_000,
+  max_concurrency: 10,
+  every: 0
 
 import_config "#{config_env()}.exs"
--- config/runtime.exs
+++ config/runtime.exs
@@ -1,5 +1,1 @@
 import Config
-
-config :load_fest_book_club, LoadFestBookClub.Looper,
-  max_concurrency: String.to_integer(System.get_env("MAX_CONCURRENCY", "1")),
-  every: String.to_integer(System.get_env("EVERY", "1000"))
