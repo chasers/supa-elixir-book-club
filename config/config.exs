939d312a6d8fe6b289a023ff7625582716691aee
Chase Granberry <chase@logflare.app>
2022-05-17 10:21:23 -0700

    Faster

--- config/config.exs
+++ config/config.exs
@@ -2,7 +2,7 @@ import Config
 
 config :load_fest_book_club, LoadFestBookClub.Looper,
   stream_len: 1_000,
-  max_concurrency: 10,
+  max_concurrency: 50,
   every: 0
 
 import_config "#{config_env()}.exs"
