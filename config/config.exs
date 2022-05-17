d9bfae9cc058613d299c3bc0a2fff341be3ad06a
Chase Granberry <chase@logflare.app>
2022-05-17 09:54:09 -0700

    This needs to be known at compile time

--- config/config.exs
+++ config/config.exs
@@ -1,7 +1,5 @@
 import Config
 
-config :load_fest_book_club, LoadFestBookClub.Looper,
-  every: 1_000,
-  tasks: 10
+config :load_fest_book_club, LoadFestBookClub.Looper, stream_len: 1
 
 import_config "#{config_env()}.exs"
--- config/runtime.exs
+++ config/runtime.exs
@@ -2,5 +2,4 @@ import Config
 
 config :load_fest_book_club, LoadFestBookClub.Looper,
   max_concurrency: String.to_integer(System.get_env("MAX_CONCURRENCY") || "1")
-  stream_len: String.to_integer(System.get_env("STREAM_LEN") || "1")
   every: String.to_integer(System.get_env("EVERY") || "1000")
