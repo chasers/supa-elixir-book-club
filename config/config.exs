f43304486db3de4070aeedb300a92261154cfca1
Chase Granberry <chase@logflare.app>
2022-05-17 09:59:57 -0700

    Syntax err

--- config/config.exs
+++ config/config.exs
@@ -1,5 +1,5 @@
 import Config
 
-config :load_fest_book_club, LoadFestBookClub.Looper, stream_len: 1
+config :load_fest_book_club, LoadFestBookClub.Looper, stream_len: 1000
 
 import_config "#{config_env()}.exs"
